7565f876d8d0e6054f3e0508a822fd993241e6bc
Bin-Jing <jeff20001234@yahoo.com.tw>
2018-12-03 18:11:28 +0800

    first commit

---
+++ seg.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import skimage
+
+def cutImg(name):
+
+    img = cv2.imread(name)
+    side = 1200
+    ratio = float(side) / max(img.shape)
+    img = skimage.img_as_ubyte(
+                               skimage.transform.resize(
+                                                        img, (int(img.shape[0] * ratio), int(img.shape[1] * ratio))))
+    x = 300
+    y = 0
+    w = 300
+    h = 1140
+
+    raw_img = img[y:y+h, x:x+w]
+#    cv2.imshow("raw", raw_img)
+    cv2.imwrite('raw.jpg', raw_img)
+
+    store_y = 0
+    store_h = 60
+
+    store_img = raw_img[store_y:store_y+store_h, 0:w]
+    cv2.imwrite('store.jpg', store_img)
+
+    date_y = store_h
+    date_h = 50
+
+    date_img = raw_img[date_y:date_y+date_h, 0:w]
+    cv2.imwrite('date.jpg', date_img)
+
+    code_y = date_y + date_h
+    code_h = 120
+
+    code_img = raw_img[code_y:code_y+code_h, 0:w]
+    cv2.imwrite('code.jpg', code_img)
+
+    cash_y = code_y + code_h
+    cash_h = 660
+
+    cash_img = raw_img[cash_y:cash_y+cash_h, 0:w]
+    cv2.imwrite('cash.jpg', cash_img)
+
+cutImg("img.jpg")
